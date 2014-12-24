a5dfb0dff8c2b8f7775bf154dde1bea3fd9fb52f
Katrina <katrina@Katrinas-MacBook-Pro.local>
2014-12-23 22:37:57 -0500

    Commenting code

--- header.php
+++ header.php
@@ -16,18 +16,27 @@
 		<script types="text/javascript" src="js/greensock/TweenMax.min.js"></script>		
 		<script src="js/jquery.superscrollorama.js"></script>		
 		<script src="js/jquery.smooth-scroll.min.js"></script>	
-		<script src="js/script.js"></script>	
+		<script src="js/script.js"></script>
 	</head>
 	
 	<body>
 		<div id="pagewrap">
 
+			<!--row-->
 			<header class="header section group">
+				<!-- desktop version of header-->
 				<nav class="desktop">
+					<!--4/12 column
+						TORONTO HYDRO LOGO
+					-->
 					<div class="col span_4_of_12">
-						<a href="http://www.torontohydro.com"><img id="THlogo" class="logos" src="img/th_logo.png" alt="Toronto Hydro"></a>
+						<a href="http://www.torontohydro.com"><img id="THlogo" class="logos" src="img/th_logo.svg" alt="Toronto Hydro logo" onerror="this.onerror=null;this.src='img/th_logo.png'"></a>
 					</div>
+					<!-- -->
 
+					<!--4/12 column
+						NAVIGATION
+					-->
 					<div class="col span_4_of_12">
 						<center>
 						<ul>
@@ -36,21 +45,39 @@
 						</ul>
 						</center>
 					</div>
+					<!-- -->
 
+					<!--4/12 column
+						SAVE ON ENERGY LOGO
+					-->
 					<div class="col span_4_of_12">
 						<a href="https://www.saveonenergy.ca/"><img id="SoElogo" class="logos" src="img/soe_logo.png" alt="Save on Energy"></a>
 					</div>
+					<!-- -->
+
 				</nav>
+				<!-- -->
 
 				<!-- mobile version of header-->
 				<nav class="mobile">
+					<!--row-->
 					<div class="section group logos">
+						<!--12/12 column
+							BOTH LOGOS
+						-->
 						<div class="col span_12_of_12">
-							<a href="http://www.torontohydro.com"><img id="THlogo" class="logos" src="img/th_logo.png" alt="Toronto Hydro"></a>
+							<a href="http://www.torontohydro.com"><img id="THlogo" class="logos" src="img/th_logo.svg" alt="Toronto Hydro logo" onerror="this.onerror=null;this.src='img/th_logo.png'"></a>
 							<a href="https://www.saveonenergy.ca/"><img id="SoElogo" class="logos" src="img/soe_logo.png" alt="Save on Energy"></a>
 						</div>
+						<!-- -->
 					</div>
+					<!--end row-->
+
+					<!--row-->
 					<div class="section group">
+						<!--12/12 column
+							NAVIGATION
+						-->
 						<div class="col span_12_of_12 mobilenav">
 							<center>
 							<ul>
@@ -59,8 +86,11 @@
 							</ul>
 							</center>
 						</div>
+						<!-- -->
 					</div>
+					<!--end row-->
 				</nav>
-
 				<!-- -->
+
 			</header>
+			<!--end row-->
--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?include 'header.php';?>
 	
+	<!-- row -->
 	<div class="section group" id="intro-banner">
 		<span>
+		<!-- 12/12 column -->
 		<div class="col span_12_of_12">
 
-			<!--background sizing div-->
+			<!--Background sizing div: 
+				Using an image of the background placed as an <img> to adjust the height of the div appropriately as it scales with it. 
+			-->
 			<img id="filler" src="img/intro-banner-bg.png" style="visibility: hidden; width: 100%; height: auto;">
 			<!--background sizing div-->
 
 			<div class="content">
+				<!-- ENERGY ALL STAR title image -->
 				<img src="img/energy-alllstar-wordmark.png" alt="Energy All Star: Jamaal Magloire" id="title-mark">
+				<!-- -->
 
+				<!-- row -->
 				<div class="section group">
+					<!-- 6/6 column (CSS floats this to the right for desktop) -->
 					<div class="col span_6_of_12">
 						<div id="intro-text">
 							Toronto Hydro and Jamaal have teamed up with tips and programs to help you save at home and at work.
@@ -19,11 +27,16 @@
 						<span class="intro-h1">THIS IS YOUR</span>
 						<h1>ENERGY-SAVING PLAYBOOK</h1>
 					</div>
+					<!-- end 6/6 column -->
 				</div>
+				<!-- end row -->
+
 			</div>
 		</div>
+		<!-- end 12/12 column -->
 
 		</span>
 	</div>
+	<!-- end row -->
 	
 	<?include 'footer.php'; ?>
